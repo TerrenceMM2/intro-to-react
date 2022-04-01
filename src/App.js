d96fd1e7e969d44f6e49d347d8029ad23b1feb5d
Terrence Mahnken <terrencemm2@gmail.com>
2022-04-01 17:52:46 -0500

    feat: add all modules to App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import ModuleOne from "./components/ModuleOne";
+import ModuleTwo from "./components/ModuleTwo/ParentComponent";
+import ModuleThree from "./components/ModuleThree/MainComponent";
+import ModuleFour from "./components/ModuleFour";
+
+// Comment/Uncomment each Module
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* JSX */}
+      {/* <ModuleOne /> */}
+      {/* State & Props */}
+      {/* <ModuleTwo /> */}
+      {/* Function & Class Components */}
+      {/* <ModuleThree /> */}
+      {/* HTML & CSS in React */}
+      <ModuleFour />
     </div>
   );
 }
